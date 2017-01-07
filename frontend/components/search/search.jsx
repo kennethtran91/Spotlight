9d0b6878bd97f4ab79d88fe18e3aac047716b21e
Lindsey Whitley <lewhitley@gmail.com>
2017-01-06 16:55:06 -0800

    fix search issues with cursor and empty on close

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -11,6 +11,7 @@ class Search extends React.Component {
 
     this.handleSearch = this.handleSearch.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   handleSearch (e) {
@@ -29,11 +30,17 @@ class Search extends React.Component {
     this.props.clearSearchTracks();
   }
 
+  handleClose(e) {
+    e.preventDefault();
+    this.props.closeModal();
+    this.props.clearSearchTracks();
+  }
+
   render() {
     return (
       <div className='search'>
-        <button className='close-search' onClick={this.props.closeModal} >x</button>
-        <input type='text' className='search-input' onChange={this.handleSearch} placeholder='Search lyrics and titles' value={this.state.searchParams}/>
+        <button className='close-search' onClick={this.handleClose} >x</button>
+        <input autoFocus type='text' className='search-input' id='search-input' onChange={this.handleSearch} placeholder='Search lyrics and titles' value={this.state.searchParams}/>
         <div className='search-results'>
           <ul className='search-list'>
             {this.props.searchTracks.map( (track, idx) => (
